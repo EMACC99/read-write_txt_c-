ea3652eb9d1e750810a02e5be7961120aa70bac7
Eduardo Ceja <50022572+EMACC99@users.noreply.github.com>
2019-10-31 22:21:58 -0600

    Add files via upload

---
+++ Split_string.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include<sstream>
+#include<iterator>
+#include<vector>
+#include "menu_txts.h"
+
+
+template<char delimiter>
+class delimited_by_char: public std::string{};
+
+
+std::vector <std::string> splitted_by_char(std::string &str){
+    std::istringstream iss(str);
+    const char delimiter = ' '; //puedes cambiar el valor para que separe por otras cosas
+    std::vector<std::string> splitted ((std::istream_iterator<delimited_by_char<delimiter>>(iss)), std::istream_iterator<delimited_by_char<delimiter>>());
+
+    return splitted;
+
+} 
